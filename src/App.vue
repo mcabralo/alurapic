c51a7f66b15e1dd9af3bf3a96bc2987a5f20f7b0
Matheus Cabral <44450101+mcabralo@users.noreply.github.com>
2021-02-11 17:59:51 -0300

    Ajustes no App + Slot

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
     <!-- outra forma de fazer isto é por usar um v-text (<h1 v-text="titulo></h1>) -->
 
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo" />
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-bind:key="foto" v-for="foto of fotos">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
       </li>
     </ul>
     <!-- 
@@ -17,7 +19,14 @@
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel";
+
 export default {
+
+  components: {
+    'meu-painel': Painel
+  },
+
   data() {
     return {
       titulo: "Alurapic",
@@ -26,12 +35,39 @@ export default {
   },
 
   created() {
-
-    this.$http.get('http://localhost:3000/v1/fotos')
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
       .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
+      .then(
+        fotos => (this.fotos = fotos),
+        err => console.log(err)
+      );
   }
 };
 </script>
 
-<style></style>
+<style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+
+</style>
